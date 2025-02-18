e2e7b9571fa2a6f7c19a34002c997924ae130949
Chiraphat-t <chiraphat.ttt@gmail.com>
2025-02-18 15:27:52 +0700

    01

--- average.cpp
+++ average.cpp
@@ -1,1 +1,21 @@
-
+#include<iostream>
+using namespace std;
+int main(int N,char *r[]){
+    double a=0;
+    for (int i = 0; i < N; i++)
+    {
+        a += atof(r[i]);
+    }
+    if (N<=1)
+    {
+        cout << "Please input numbers to find average.";
+    }
+    
+    else {
+        cout << "---------------------------------\n";
+        cout << "Average of "<< N-1 << " numbers = " << a/(N-1)<< endl;
+        cout << "---------------------------------\n";
+    }
+        
+     return 0;
+}
